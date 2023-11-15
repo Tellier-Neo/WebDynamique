9a89f9acf17d084f4abaee2eaf646fc3e90426a9
Tellier-Neo <blume.unit@gmail.com>
2023-11-15 09:04:52 +0100

    V3

--- exo1.js
+++ exo1.js
@@ -2,5 +2,12 @@ alert("Hello World");
 var MavariableString = "Toto";
 var MavariableNumerique = 100;
 var Mavariabletableau = ['Apple' , 'Banana'];
-var MavariableObjet = window
+var MavariableObjet = window //window.innerWidth et window.innerHeight
 alert(MavariableString + MavariableNumerique + Mavariabletableau + MavariableObjet.location.pathname);
+
+
+
+
+
+//exo 4 : 
+//En lisant ce code on sait que l'on doit entrer une valeur inférieure ou egale a 100 pour avoir le cas 2
